db34a93249c21a668d35aa2d28f63b1d84ea0d63
Marcin Czernecki <marcin@mczernecki.pl>
2025-08-03 22:03:19 +0200

    Adding semantic kernel

--- Agent/LedgerAI.Agent/Program.cs
+++ Agent/LedgerAI.Agent/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.SemanticKernel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName: builder.Configuration["AzureOpenAI:Deployment"],
+    endpoint: builder.Configuration["AzureOpenAI:Endpoint"],
+    apiKey: builder.Configuration["AzureOpenAI:ApiKey"]
+);
 
 var app = builder.Build();
 
